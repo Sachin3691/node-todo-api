c4f4d69aa0403ff8a184b9fc5edafa18b18cc226
Sachin Thomas <Sachin.Thomas@aptean.com>
2018-08-27 15:35:00 +0530

    Adding mongodb config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose =  require('mongoose');
 
+
+
+
 mongoose.Promise = global.Promise; // Setup to use Promise instead of Callbacks
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {
     mongoose: mongoose
