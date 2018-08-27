07b504e29c2e8f01462222cacf73795619eabad1
Sachin Thomas <Sachin.Thomas@aptean.com>
2018-08-27 16:45:36 +0530

    Added Patch API's

--- playground/mongoose-delete.js
+++ playground/mongoose-delete.js
@@ -3,8 +3,8 @@ const { mongoose } = require('../server/db/mongoose');
 
 
 
-const { Todo } = require('./../server/models/todo');
-const { User } = require('./../server/models/user');
+const { Todo } = require('../server/models/todo');
+const { User } = require('../server/models/user');
 
 
 //Todo.remove({}) - removes everything
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -46,7 +47,7 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send('Invalid ID');
     }
     Todo.findById(id).then((todo) => {
-        if(!todo) {
+        if (!todo) {
             return res.status(404).send('No data');
         }
         res.status(200)
@@ -63,7 +64,7 @@ app.delete('/todo/:id', (req, res) => {
     };
 
     Todo.findByIdAndRemove(id).then((todo) => {
-        if(!todo) {
+        if (!todo) {
             return res.status(404).send();
         }
         res.status(200)
@@ -72,6 +73,34 @@ app.delete('/todo/:id', (req, res) => {
         return res.status(400).send();
     });
 });
+
+
+app.patch('/todo/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed'])
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Invalid ID');
+    };
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send()
+            }
+            res.send({ todo })
+        }).catch((e) => {
+            res.status(400).send()
+        })
+
+})
 
 app.listen(port, () => {
     console.log(`Started on server port ${port}`);
